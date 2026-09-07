927ab09c8025003cfe3179d230f1b55ec0e9c1fc
alorchaya2005 <alorchaya2005@gmail.com>
2026-09-08 03:35:11 +0600

    Update useAuthStore.js

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -78,7 +78,7 @@ export const useAuthStore = create((set, get) => ({
   connectSocket: () => {
     const { authUser } = get();
     if (!authUser || get().socket?.connected) return;
-    const socket = io("https://fansign.onrender.com", {
+    const socket = io("https://fansign-46gd.onrender.com", {
       query: { userId: authUser._id },
     });
     socket.connect();
