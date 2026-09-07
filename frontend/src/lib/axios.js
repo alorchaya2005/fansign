1cde0cdb4dcf8a435275ebda1176efa71dcde16e
alorchaya2005 <alorchaya2005@gmail.com>
2026-09-08 03:17:59 +0600

    Update axios base URL to production endpoint

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000/api/musk",
+  baseURL: "https://fansign-46gd.onrender.com/api/musk",
   withCredentials: true,
 });
